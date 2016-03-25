8aaf3e572982a786ae1d9b365bc93c0ed357360c
Michael Hernandez <michael.hernandez1988@gmail.com>
2016-03-25 19:56:56 -0400

    pretty

--- app/main.jsx
+++ app/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.jsx'
 import GlobalTopNavigation from './components/GlobalTopNavigation.jsx'
 
 ReactDOM.render(<GlobalTopNavigation />, document.getElementById('root'));
